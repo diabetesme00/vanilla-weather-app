3cf59bf66fd41a159ad1e0b93e6cbd211a781c1b
diabetesme00 <54970784+diabetesme00@users.noreply.github.com>
2021-02-27 20:59:45 -0600

    update api url/key

--- src/index.js
+++ src/index.js
@@ -81,8 +81,8 @@ function dispalyForecast(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "bc6cdcca437f368fd1be932e088b3b40";
+  let apiUrl = https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemperature);
 
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
